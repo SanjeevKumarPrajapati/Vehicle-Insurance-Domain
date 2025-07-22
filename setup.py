7d151cb06fd1a37c948627b4f2ffcb23ebf2e61f
Sanjeev Kumar Prajapati <sp6406919@gmail.com>
2025-07-22 12:06:17 +0530

    Normalize line endings

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sanjeev Kumar Prajapati",
+    author_email="iamsanjeevkumar.prajapati@gmail.com",
+    packages=find_packages()
+)
